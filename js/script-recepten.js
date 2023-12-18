acb7e8cfcb7bd2f28add21bc400894f95bed60c8
didierpetit <didier.petit@student.ap.be>
2023-12-18 15:14:21 +0100

    aanmaak recepten
    aanpassingen  navigatie andere pagina's

--- js/script-recepten.js
+++ js/script-recepten.js
@@ -4,31 +4,32 @@ Zoek naar de correct url met endpopint waarbij je 20 resultaten van hot-coffee-r
 Test of je de correcte endpoint hebt in Postman. Pas nadien "fetch" je de correcte link naar de API in de code hieronder.
 */
 
-fetch('https://api.sampleapis.com/coffee/hot')
+fetch('https://api.sampleapis.com/coffee/hot/?results=20')
     // maak van het antwoord een JSON antwoord
     .then(response => response.json())
     // lees de json uit en zet om naar HTML
     .then(json => {
         // lees het volledige antwoord uit in de console (verwijder deze regel code wanneer je klaar bent)
-        console.log(json);
         // plaats een section met grid
         let html = '<div class="accordion accordion-flush" id="accordionExample">';
         // plaats van de recepten met naam, ingerdiënten, afbeelding,... in HTML
         // op regel 25 plaats je tussen de accolades de titel van het recept in
         // op regel 31 laadt je de juiste "keys" van het recept in.
-        for (let i = 0; i < json.length-2; i++) {
-            const recipe = json[i];
+        for (let recipe of json) {
+            
             html += `<div class="accordion-item">
 
             <h2 class="accordion-header">
-                <button class="accordion-button collapsed text-uppercase fs-6 fw-bolder pt-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
-                ${}
+                <button class="accordion-button collapsed text-uppercase fs-6 fw-bolder pt-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${recipe.id}" aria-expanded="false" aria-controls="collapse${recipe.id}">
+                ${recipe.title}
                 </button>
               </h2>
-
-              <div id="collapse${i}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div id="collapse${recipe.id}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
               <div class="accordion-body">
-              <img src="      " class="float-start" alt="        "><span class="h6 mt-2">ingrediënten</span>:            <hr><span class="h6  mt-2">bereiding</span>:         </div>
+              <img src="${recipe.image}" style="max-height: 150px;" class="float-start img-sm p-3" alt=""><span class="h6 mt-2 koffiebruin">ingrediënten</span>:
+               ${recipe.ingredients} <hr>
+               <span class="h6  mt-2 koffiebruin">bereiding</span>:
+               ${recipe.description}</div>
             </div>
       </div>`;
         }
